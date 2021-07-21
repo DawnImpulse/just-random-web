811ac3c705acc11af6b3a97b08d35588c0ed5190
Saksham Khurana <dawnimpulse@gmail.com>
2021-07-21 10:29:37 +0530

    feat(init): added vuetify & vuesax

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import Vuesax from "vuesax";
+import "vuesax/dist/vuesax.css"; //Vuesax styles
+
+Vue.use(Vuesax);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
